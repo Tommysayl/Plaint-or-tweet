980a1c87e1ec9fb56d09ab61746a076559e9f716
Mirko222 <mirko.giacchini98@gmail.com>
2020-12-06 19:05:33 +0100

    untested skeleton

---
+++ src/BernoulliNaiveBayes.py
@@ -0,0 +1,40 @@
+from StableNaiveBayes import StableNaiveBayes
+import numpy as np
+
+'''
+Naive Bayes where:
+each feature Xi has domain in {0,1}
+and each y is in {0,1}
+'''
+class BernoulliNaiveBayes(StableNaiveBayes):
+    def __init__(self):
+        self.m = 0
+        self.count_y_1 = 0 #numer of training examples where y=1
+        self.count_x_1_y_1 = None #i-th element is number of training examples where (Xi=1 and y=1) 
+        self.count_x_1_y_0 = None #i-th element is number of training examples where (Xi=1 and y=0)
+
+    #X and y must be numpy arrays
+    #each row of X is a training data
+    #Note: you can also call this many times (separating the dataset into batches)
+    def train(self, X, y):
+        if self.count_x_1_y_0 is None: #first time called
+            self.count_x_1_y_0 = np.zeros(len(X[0]))
+            self.count_x_1_y_1 = np.zeros(len(X[0]))
+
+        self.m += len(y)
+        self.count_y_1 += np.count_nonzero(y) #nonzero == 1
+        self.count_x_1_y_1 += np.apply_along_axis(lambda x: np.count_nonzero(x * y), 0, X) #x*y => x=1 and y=1
+        self.count_x_1_y_0 += np.apply_along_axis(lambda x: np.count_nonzero(x * (1-y)), 0, X) #x*(1-y) => x=1 and y=0
+
+    def p_xi_given_y(self, X, i, y):
+        p1 = 0
+        if y == 1:
+            p1 = self.count_x_1_y_1[i] / self.count_y_1
+        else:
+            p1 = self.count_x_1_y_0[i] / (self.m - self.count_y_1)
+
+        return p1 if X[i] == 1 else (1 - p1)
+
+    def p_y(self, y):
+        p1 = self.count_y_1 / self.m
+        return p1 if y == 1 else (1 - p1)
---
+++ src/StableNaiveBayes.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from math import log
+import numpy as np
+
+'''
+abstract naive bayes class, but numerically stable (uses logs of probabilities)
+we assume y integer in {0, 1} (binary classification)
+'''
+class StableNaiveBayes(ABC):
+    #X = list of training vectors
+    #y = vector of classes
+    @abstractmethod
+    def train(self, X, y):
+        pass
+
+    #compute probability of X[i] given y
+    @abstractmethod
+    def p_xi_given_y(self, X, i, y):
+        pass
+    
+    #compute probability of y
+    @abstractmethod
+    def p_y(self, y):
+        pass
+    
+    '''
+    computes (dropping the denominator) log(P(y|X)) = 
+    (bayes + den. drop) => log(P(X|y) P(y)) = sum(log(P(Xi|y)) + log(P(y))
+    '''
+    def log_prob_y_given_x(self, X, y):
+        return sum([log(self.p_xi_given_y(X, i, y)) for i in range(len(X))]) + log(self.p_y(y))
+    
+    #predict y for data X
+    def predict_class(self):
+        p0 = self.log_prob_y_given_x(X, 0)
+        p1 = self.log_prob_y_given_x(X, 1)
+        return 0 if p0 > p1 else 1
