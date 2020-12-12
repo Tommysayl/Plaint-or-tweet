1047c4b630e2854722d3183de920a439db7f0598
Tommaso Battistini <tommy.batti@hotmail.com>
2020-12-12 13:35:12 +0100

    Embedder

---
+++ src/datasets/Embedder.py
@@ -0,0 +1,32 @@
+from gensim.models import FastText 
+from gensim.models import Word2Vec
+import pandas as pd
+
+
+class Embedder():
+
+    def __init__(self):
+        self.corpus = []
+
+    def load_corpus(self, csv_path):
+        df = pd.read_csv(csv_path)
+        self.corpus = df.iloc[:,2].values
+        self.y = df['Label'].values
+        return self.corpus, self.y
+        
+
+    def embed_ft (self, csv_path , size, window, min_count, workers, sg):
+        corpus, tags = self.load_corpus(csv_path)
+        word_corpus = [sentence.lower().split() for sentence in corpus if isinstance(sentence, str)]
+        model = FastText(sentences = word_corpus, size = size, window = window, min_count = min_count, workers = workers, sg = sg)
+        return model
+
+    def embed_w2v (self,csv_path, size, window, min_count, workers, sg):
+        corpus, tags = self.load_corpus(csv_path)
+        word_corpus = [sentence.lower().split() for sentence in corpus if isinstance(sentence, str)]
+        model = Word2Vec(sentences = word_corpus, size = size, window = window, min_count = min_count, workers = workers, sg = sg)
+        return model  
+        
+
+
+    
---
+++ src/experiments/TestEmbeddings.py
@@ -0,0 +1,34 @@
+from src.datasets.Embedder import Embedder
+import time
+
+start_time = time.time()
+csv = "bow_preprocess.csv"
+vector_size = 100
+window_size = 5
+sg = 0
+min_count = 1
+workers = 1
+
+embedder = Embedder()
+print("creating FastText model... ")
+ft_model = embedder.embed_ft(csv, vector_size, window_size, min_count, workers, sg)
+print("Done")
+print("Time needed:", (time.time() - start_time))
+print("Creating Word2Vec model...")
+w2v_model = embedder.embed_w2v(csv, vector_size, window_size, min_count, workers, sg)
+print("Done")
+
+print("Time needed:", (time.time() - start_time))
+print("Comparing models: science:")
+print("FastText:")
+print(ft_model.wv.most_similar("science"))
+print("Word2Vec:")
+print(w2v_model.wv.most_similar("science"))
+print("Second comparison: homework")
+print("FastText:")
+print(ft_model.wv.most_similar("homework"))
+print("Word2Vec:")
+print(w2v_model.wv.most_similar("homework"))
+print("total time for test:",(time.time() - start_time))
+
+
