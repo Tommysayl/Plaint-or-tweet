6ca81697740cf57ac9856b24ca1feabf6788af03
Tommaso Battistini <tommy.batti@hotmail.com>
2020-12-13 15:06:02 +0100

    Discretization, Comments

--- src/datasets/Embedder.py
+++ src/datasets/Embedder.py
@@ -1,6 +1,7 @@
 from gensim.models import FastText 
 from gensim.models import Word2Vec
 import pandas as pd
+import numpy as np
 
 
 class Embedder():
@@ -15,18 +16,77 @@ class Embedder():
         return self.corpus
         
 
-    def embed_ft (self, csv_path , size, window, min_count, workers, sg):
+    def embed_ft (self, csv_path , size = 100, window = 5, min_count = 1, workers = 1, sg = 1):
+        '''
+        Input: Path to csv;
+        size = embedding vector size;
+        window = Maximum distance between the current and predicted word within a sentence
+        min_count =  Ignores all words with total frequency lower than this.
+        workers = threads to train model
+        sg = 0 for CBOW, 1 for skip-gram
+        Output: Gensim Model
+        '''
         corpus = self.load_corpus(csv_path)
         word_corpus = [sentence.lower().split() for sentence in corpus if isinstance(sentence, str)]
         model = FastText(sentences = word_corpus, size = size, window = window, min_count = min_count, workers = workers, sg = sg)
         return model
 
-    def embed_w2v (self,csv_path, size, window, min_count, workers, sg):
+    def embed_w2v (self, csv_path , size = 100, window = 5, min_count = 1, workers = 1, sg = 0):
+        '''
+        Input: Path to csv;
+        size = embedding vector size;
+        window = Maximum distance between the current and predicted word within a sentence
+        min_count =  Ignores all words with total frequency lower than this.
+        workers = threads to train model
+        sg = 0 for CBOW, 1 for skip-gram
+        Output: Gensim Model
+        '''
+        
         corpus = self.load_corpus(csv_path)
         word_corpus = [sentence.lower().split() for sentence in corpus if isinstance(sentence, str)]
         model = Word2Vec(sentences = word_corpus, size = size, window = window, min_count = min_count, workers = workers, sg = sg)
         return model  
         
+    def discretize_vectors (self, model, n_ranges = 10):
+        '''
+        Input: FastText/Word2Vec model (created with self.embed_ft/w2v), int representing the number of ranges
+        we want to discretize our values to. 
+        Output; Dictionary where Keys = words, values = discretized embedded vectors
+        '''
+        support_dict = {}
+        vector_dict = {}
+        for word in model.wv.vocab:
+            support_dict[word] = model.wv[word]
+
+        
+        for word in support_dict:
+            vector_dict[word] = list()
+            min_a, max_a = self.find_min_max(support_dict[word])
+            for value in support_dict[word]:
+                discretized = self.discretize_value(max_a, min_a, value, n_ranges)
+                vector_dict[word].append(discretized)
+        return vector_dict
+
+
+    def discretize_value(self, max_a, min_a, value, n_ranges):
+        normalized = (value - min_a) / (max_a - min_a)
+        discretized = np.round(normalized * n_ranges) 
+        return discretized
+
+    def find_min_max(self, array):
+        max_a = max(array)
+        min_a = min(array)
+        return max_a, min_a
+        
+
+
+
+
+
+        
+        
+        
+
 
 
     
--- src/experiments/TestEmbeddings.py
+++ src/experiments/TestEmbeddings.py
@@ -6,7 +6,7 @@ csv = "bow_preprocess.csv"
 vector_size = 100
 window_size = 5
 sg = 0
-min_count = 1
+min_count = 5
 workers = 1
 
 embedder = Embedder()
@@ -15,7 +15,7 @@ ft_model = embedder.embed_ft(csv, vector_size, window_size, min_count, workers, 
 print("Done")
 print("Time needed:", (time.time() - start_time))
 print("Creating Word2Vec model...")
-w2v_model = embedder.embed_w2v(csv, vector_size, window_size, min_count, workers, sg)
+#w2v_model = embedder.embed_w2v(csv, vector_size, window_size, min_count, workers, sg)
 print("Done")
 
 print("Time needed:", (time.time() - start_time))
@@ -23,12 +23,20 @@ print("Comparing models: science:")
 print("FastText:")
 print(ft_model.wv.most_similar("science"))
 print("Word2Vec:")
-print(w2v_model.wv.most_similar("science"))
+#print(w2v_model.wv.most_similar("science"))
 print("Second comparison: homework")
 print("FastText:")
 print(ft_model.wv.most_similar("homework"))
 print("Word2Vec:")
-print(w2v_model.wv.most_similar("homework"))
+#print(w2v_model.wv.most_similar("homework"))
+print("Time needed:", (time.time() - start_time))
+print("Discretizing embedded vectors:")
+ciao = embedder.discretize_vectors(ft_model)
+print("Testing Embedding discretization:")
+print("Original vector for the word Science:")
+print(ft_model.wv["science"])
+print("Discretized Vector:")
+print(ciao["science"])
 print("total time for test:",(time.time() - start_time))
 
 
