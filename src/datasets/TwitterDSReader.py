f2ff3ad32ccf09d6dd466cc79e942b356b1dd2b4
edodema <edodema13@gmail.com>
2020-12-17 15:06:59 +0100

    Added twitter6ok (just for testing9 and modified 'time' label

--- src/datasets/TwitterDSReader.py
+++ src/datasets/TwitterDSReader.py
@@ -8,12 +8,13 @@ class TwitterDSReader(DatasetReader):
 
     @property
     def default_path(self) -> str:
-        return "../../datasets/training.1600000.processed.noemoticon.csv"
+        #return "../../datasets/training.1600000.processed.noemoticon.csv"
+        return "../../datasets/twitter.csv"
 
     def read_from_file(self, p=None, remove_stopwords=True, remove_links=True, correct_typos=True) -> None:
         super().read_from_file(p)
 
-        df = pd.read_csv(self.get_path(p), encoding="ISO-8859-1", names=["label", "time", "date", "query", "username", "text"])
+        df = pd.read_csv(self.get_path(p), encoding="ISO-8859-1", names=["label", "id", "date", "query", "username", "text"])
         tweets = self.preprocess(df["text"], remove_links, correct_typos)
 
         self.build_from(tweets, df["label"], remove_stopwords)
